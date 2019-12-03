608b6e1eedff23d604b1184989056e30f004f3a7
JoshuaRabito <42897385+JoshuaRabito@users.noreply.github.com>
2019-12-02 21:37:42 -0600

    Add files via upload

---
+++ backend/src/test/java/mymoneymanager/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mymoneymanager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
